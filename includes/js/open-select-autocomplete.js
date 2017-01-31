bf987a6e5139137fb55aa9c264364c8ca2984d36
Cristian Frumusanu <cristian.frumusanu@pixelgrade.com>
2017-01-31 16:56:34 +0200

    Use jQuery UIs autocomplete

--- includes/js/open-select-autocomplete.js
+++ includes/js/open-select-autocomplete.js
@@ -13,9 +13,49 @@
 				{ value: "{next-end-time}" }
 		];
 
-		$('.js-time-autocomplete').each( function() {
-			$( this ).autocomplete({
-				lookup: timeTags
+//		$('.js-time-autocomplete').each( function() {
+//			$( this ).autocomplete({
+//				lookup: timeTags
+//			});
+//		});
+
+		function split( val ) {
+			return val.split( / \s*/ );
+		}
+		function extractLast( term ) {
+			return split( term ).pop();
+		}
+
+		$( '.js-time-autocomplete' ).each( function (  ) {
+			// don't navigate away from the field on tab when selecting an item
+			$( this ).on( "keydown", function( event ) {
+				if ( event.keyCode === $.ui.keyCode.TAB &&
+				     $( this ).autocomplete( "instance" ).menu.active ) {
+					event.preventDefault();
+				}
+			})
+			.autocomplete({
+				minLength: 0,
+				source: function( request, response ) {
+					// delegate back to autocomplete, but extract the last term
+					response( $.ui.autocomplete.filter(
+						timeTags, extractLast( request.term ) ) );
+				},
+				focus: function() {
+					// prevent value inserted on focus
+					return false;
+				},
+				select: function( event, ui ) {
+					var terms = split( this.value );
+					// remove the current input
+					terms.pop();
+					// add the selected item
+					terms.push( ui.item.value );
+					// add placeholder to get the comma-and-space at the end
+					terms.push( "" );
+					this.value = terms.join( " " );
+					return false;
+				}
 			});
 		});
 	});
